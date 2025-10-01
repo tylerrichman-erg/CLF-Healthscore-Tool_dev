edc2efc859dfd365f63cd9fb045dc1a86c238963
Tyler Richman <tyler.richman@erg.com>
2025-10-01 10:26:10 -0400

    Update data_service.py

--- automator/healthscore/services/data_service.py
+++ automator/healthscore/services/data_service.py
@@ -3,6 +3,7 @@ from django.db import transaction
 from healthscore.data.ACSDataLoader import ACSDataLoader
 from healthscore.data.CDCDataLoader import CDCDataLoader
 from healthscore.data.EJScreenDataLoader import EJScreenDataLoader
+from healthscore.data.AirToxScreenDataLoader import AirToxScreenDataLoader
 from healthscore.models import Metric, State, Dataset, DataSource
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -25,6 +26,7 @@ class DataService:
             'CDC': '2022',
             'BRFSS': '2022',
             'EJScreen': '2024',
+            'AirToxScreen': '2020',
             'LifeExpectancy': '2018',
             'Latch': 'Nov 23, 2018',
             'SmartLocation': 'Jan, 2021',
@@ -492,5 +494,156 @@ class DataService:
         all_geoids = [state.fips_code] + geoids
         return Metric.objects.filter(
             dataset__data_source__name__in=ej_loader.datasources.keys(), dataset__vintage=vintage,
+            geoid__in=all_geoids
+        )
+
+    @transaction.atomic
+    def load_ats(self, vintage: str, state: str, county: str, tracts: list):
+        """
+        :param vintage: i.e. '2019'
+        :param state: i.e. 'MA'
+        :param county: i.e. '009'
+        :param tracts: i.e. ['215102', '215101', '216100']
+        :return: Metric objects corresponding to the requested data
+        """
+        all_records = []
+
+        # Find the state
+        try:
+            state = State.objects.get(short_code=state)
+        except ObjectDoesNotExist:
+            self.log.error("Couldn't find state with abbreviation " + state)
+            return all_records
+
+        ats_loader = AirToxScreenDataLoader(vintage, self.user)
+
+        for d in ats_loader.datasources.keys():
+
+            # Find the datasource (i.e. 'EJ Screen')
+            try:
+                datasource = DataSource.objects.get(name=d)
+            except ObjectDoesNotExist:
+                self.log.error("Couldn't find data source " + d)
+                return all_records
+
+            # Load (creating first if necessary) the dataset for this source/vintage combination.
+            try:
+                dataset = Dataset.objects.get(data_source__name=d, vintage=vintage)
+            except ObjectDoesNotExist:
+                self.log.info(f'Dataset for {datasource.name} / {vintage} does not exist. Creating...')
+                dataset = Dataset.objects.create(data_source=datasource, vintage=vintage,
+                                                 descriptor="AirToxScreen environmental data vintages vary. Check datasource reference URL for more information.")
+
+            # Load the metrics so that we can perform a reverse lookup on the census code if we end up needing
+            # to fetch any of the geoids from the API.
+            # (i.e. 'B01003_001' -> 'Total Population')
+            metrics = ats_loader.metrics[d]
+
+            state_data = {}
+
+            # State-level data will be returned along with tract level data...
+            # Fetch and persist tract-level data if not already present
+            for tract in tracts:
+
+                geoid = state.fips_code + county + tract
+
+                metric_exists = {k: Metric.objects.filter(
+                    dataset__data_source__name=d, dataset__vintage=vintage,
+                    geoid=geoid, code__contains=v).exists() for k, v in metrics.items()}
+
+                if not all(metric_exists.values()):
+
+                    try:
+                        fetched_df = ats_loader.fetch_tract_data(d, state.fips_code, county, tract)
+
+                        # Only iterate the columns that represent values (i.e. that end in 'E')
+                        # The corresponding margin of error will added to the same record.
+                        value_columns = fetched_df.loc[:, fetched_df.columns.str.startswith('RAW_E')]
+
+                        records = []
+
+                        row_iter = fetched_df.iterrows()
+                        for index, row in row_iter:
+                            for value_col in value_columns:
+
+                                code = value_col[6:]
+                                metric_name = list(metrics.keys())[list(metrics.values()).index(code)]
+
+                                if metric_exists[metric_name]:
+                                    continue
+
+                                # Save the state value for later
+                                state_avg_col = 'S_E_' + code
+                                state_avg = fetched_df[state_avg_col][index]
+                                state_data[state_avg_col] = state_avg
+
+                                est = fetched_df[value_col][index]
+                                records.append(
+                                    Metric(
+                                        state_id=state.id,
+                                        dataset_id=dataset.id,
+                                        geoid=state.fips_code + county + tract,
+                                        name=metric_name,
+                                        code=value_col,
+                                        value=est,
+                                        moe=None
+                                    )
+                                )
+
+                                percentile_col = 'S_P_' + code
+                                percentile = fetched_df[percentile_col][index]
+                                records.append(
+                                    Metric(
+                                        state_id=state.id,
+                                        dataset_id=dataset.id,
+                                        geoid=state.fips_code + county + tract,
+                                        name='Percentile ' + metric_name,
+                                        code=percentile_col,
+                                        value=percentile,
+                                        moe=None
+                                    )
+                                )
+                        Metric.objects.bulk_create(records)
+                    except BaseException:
+                        print("Error fetching AirToxScreen missing metrics for tract: " + tract)
+                        return all_records
+
+            # Persist state-level data if not already present (note this data was fetched
+            # along with the tract-level data...)
+            metric_exists = {k: Metric.objects.filter(
+                dataset__data_source__name=d, dataset__vintage=vintage,
+                geoid=state.fips_code, code__contains=v).exists() for k, v in metrics.items()}
+            if not all(metric_exists.values()):
+
+                records = []
+
+                for key in state_data:
+
+                    code = key[4:]
+                    metric_name = list(metrics.keys())[list(metrics.values()).index(code)]
+
+                    if metric_exists[metric_name]:
+                        continue
+
+                    value = state_data[key]
+
+                    records.append(
+                        Metric(
+                            state_id=state.id,
+                            dataset_id=dataset.id,
+                            geoid=state.fips_code,
+                            name=metric_name,
+                            code=key,
+                            value=value,
+                            moe=None
+                        )
+                    )
+                Metric.objects.bulk_create(records)
+
+        # Return the Metric records for both state and tracts
+        geoids = [state.fips_code + county + t for t in tracts]
+        all_geoids = [state.fips_code] + geoids
+        return Metric.objects.filter(
+            dataset__data_source__name__in=ats_loader.datasources.keys(), dataset__vintage=vintage,
             geoid__in=all_geoids
         )
